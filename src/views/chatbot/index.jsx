f6533e1298653e03f4f5ab4f54b98d2cea2928b4
jeong617 <loodol99@naver.com>
2024-08-29 22:51:51 +0900

    feat: update stata management

--- src/views/chatbot/index.jsx
+++ src/views/chatbot/index.jsx
@@ -4,24 +4,31 @@ import React from "react";
 function ChatBot() {
     const [chatHistory, setChatHistory] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    const [userId, setUserId] = useState('');
+    useEffect(() => {
+        console.log("chat history 변경: ", chatHistory);
+    }, [chatHistory]);
 
     const handleInputChange = (event) => {
         setInputValue(event.target.value);
     };
+    
+    const handleUserIdChange =(e) => {
+        setUserId(e.target.value);
+    }
 
     /* url로 요청 */
     const sendMessage = async (event) => {
         event.preventDefault();
 
-        const newMessage = { sender: 'user', message: inputValue };
+        const newMessage = { sender: `${userId}`, message: inputValue };
+        const data = {
+            message: inputValue,
+        };
         setChatHistory([...chatHistory, newMessage]);
 
-        try {            
-            const userId = 'SpicyGirl20';   // 임의 값
+        try {
             const url = `https://moreburger.org/api/chatbot/${userId}`;
-            const data = {
-                message: inputValue,
-            };
 
             const options = {
                 method: 'POST',
@@ -47,7 +54,7 @@ function ChatBot() {
             const errorMessage = { sender: 'bot', message: "Failed to get a response from the server." };
             setChatHistory(prevHistory => [...prevHistory, errorMessage]);
         }
-        console.log(chatHistory);
+
         setInputValue('');
     };
 
@@ -62,6 +69,13 @@ function ChatBot() {
             </div>
 
             <form onSubmit={sendMessage}>
+                <input 
+                    type="text" 
+                    value={userId} 
+                    onChange={handleUserIdChange} 
+                    placeholder="userId" 
+                />
+
                 <input 
                     type="text" 
                     value={inputValue} 
