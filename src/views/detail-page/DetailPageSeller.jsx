37f83ce090b347ad9fdc33676c3f89dca7c82991
jeong617 <loodol99@naver.com>
2024-08-29 15:18:09 +0900

    feat: update detail page config

--- src/views/detail-page/DetailPageSeller.jsx
+++ src/views/detail-page/DetailPageSeller.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 
 // material-ui
-import { Grid, CardMedia, Typography, Button, Divider } from '@mui/material';
+import { Grid, CardMedia, Button, Divider, InputLabel, TextField, Input } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+
 
 /* 기존 상품 정보 불러오기 */
 
@@ -21,6 +22,19 @@ function DetailPageSeller() {
     const theme = useTheme();
     const [imagePreview, setImagePreview] = useState(null);
 
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        
+        reader.onloadend = () => {
+          setImage(reader.result);
+        };
+        
+        if (file) {
+          reader.readAsDataURL(file);
+        }
+      };
+
     const handleUpdate = (values) => {
         alert("업데이트 완료!");
     }
@@ -34,7 +48,7 @@ function DetailPageSeller() {
     return (
         <Formik
         initialValues={{
-            /* 기존 상품 정보 대입 */
+            /* TODO: 기존 상품 정보 대입 */
             productName: "",
             productInfo: "",
             allergyInfo: "",
@@ -43,89 +57,136 @@ function DetailPageSeller() {
         validationSchema={validationSchema}
         onSubmit={(values) => handleUpdate(values)}
         >
-            <Grid container spacing={2}
-                sx={{
-                    padding: '24px 8rem 24px 8rem',
-                    width: '100%',
-                    // minWidth: '600px'
-                }}>
-            {/* 브랜드명 */}
-            <Grid item xs={12} sx={{marginBottom: 5}}>
-                <Typography sx={theme.typography.h2} gutterBottom>
-                브랜드 명
-                </Typography>
-                <Divider />
-            </Grid>
+            <Form>
+                <Grid container spacing={2}
+                    sx={{
+                        padding: '24px 8rem 24px 8rem',
+                        width: '100%',
+                        // minWidth: '600px'
+                    }}>
+                    {/* 브랜드명 */}
+                    <Grid item xs={12} sx={{marginBottom: 5}}>
+                        <TextField
+                            id="adornment-brand"
+                            label="브랜드명"
+                            variant="standard"
+                            type="text"
+                            sx={{marginBottom: 1}}
+                            InputProps={{
+                                disableUnderline: true, // 밑줄 제거
+                            }}
+                        />
+                        <Divider />
+                    </Grid>
 
-            {/* 좌측: 이미지 */}
-            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
-                <CardMedia
-                component="img"
-                image="/src/assets/images/menus/menu1.jpeg" // 실제 이미지 경로로 변경
-                alt="버거 사진"
-                sx={{ width: '20rem', height: '20rem' }}
-                />
-            </Grid>
+                    {/* 좌측: 이미지 */}
+                    <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <Input
+                            type="file"
+                            onChange={handleImageUpload}
+                            disableUnderline
+                        ></Input>
+                    </Grid>
+                
+                    {/* 우측: 정보 */}
+                    <Grid item xs={6}>
+                        <TextField
+                            id="adornment-burger-name"
+                            label="버거이름"
+                            variant="standard"
+                            type="text"
+                            sx={{width: '100%', marginBottom: 1}}
+                            InputProps={{
+                                disableUnderline: true, // 밑줄 제거
+                            }}
+                        />
+                        <TextField
+                            id="adornment-burger-description"
+                            label="버거설명"
+                            variant="standard"
+                            type="text"
+                            sx={{width: '100%', marginBottom: 1}}
+                            InputProps={{
+                                disableUnderline: true, // 밑줄 제거
+                            }}
+                        />
 
-            {/* 우측: 정보 */}
-            <Grid item xs={6}>
-                <Typography sx={theme.typography.h3} gutterBottom>
-                이름
-                </Typography>
-                <Typography sx={theme.typography.menuCaption} gutterBottom>
-                햄버거 설명
-                </Typography>
-                <Typography sx={theme.typography.subtitle1} gutterBottom>
-                가격: 00,000원
-                </Typography>
+                        {/* 옵션 선택 (단품, 세트) */}
+                        <Grid container spacing={2} sx={{ marginTop: 2, marginBottom: 4 }}>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="adornment-burger-single-price"
+                                label="단품 가격"
+                                variant="standard"
+                                type="number"
+                                InputProps={{
+                                    disableUnderline: true,
+                                }}
+                            ></TextField>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <TextField
+                                id="adornment-burger-set-price"
+                                label="세트 가격"
+                                variant="standard"
+                                type="number"
+                                InputProps={{
+                                    disableUnderline: true,
+                                }}
+                            ></TextField>
+                        </Grid>
+                        </Grid>
+                        <Divider sx={{ marginBottom: 2 }} />
 
-                {/* 옵션 선택 (단품, 세트) */}
-                <Grid container spacing={2} sx={{ marginTop: 2 }}>
-                <Grid item xs={6}>
-                    <Button variant="outlined" fullWidth>
-                    단품 00,000원
-                    </Button>
-                </Grid>
-                <Grid item xs={6}>
-                    <Button variant="outlined" fullWidth>
-                    세트 00,000원
-                    </Button>
-                </Grid>
-                </Grid>
+                        {/* 알레르기 정보 */}
+                        <TextField
+                            id="adornment-burger-allergy"
+                            label="알러지 정보"
+                            variant="standard"
+                            type="text"
+                            sx={{width: "100%", marginBottom: 2}}
+                            InputProps={{
+                                disableUnderline: true,
+                            }}
+                        ></TextField>
 
-                {/* 알레르기 정보 */}
-                <Typography  sx={{ marginTop: 5 }}>
-                알레르기 정보
-                </Typography>
-                <Divider sx={{ marginBottom: 2 }} />
-                <Typography variant="subtitle2" sx={{ marginTop: 2 }}>
-                서버에서 받아온 알레르기 정보
-                </Typography>
+                        {/* 칼로리 정보 */}
+                        <TextField
+                            id="adornment-burger-calories"
+                            label="칼로리 정보"
+                            variant="standard"
+                            type="number"
+                            sx={{width: "100%"}}
+                            InputProps={{
+                                disableUnderline: true,
+                            }}
+                        ></TextField>
 
-                {/* 장바구니 및 구매 버튼 */}
-                <Grid container spacing={2} sx={{ marginTop: 2 }}>
-                <Grid item xs={6}>
-                    <Button
-                        type="submit"
-                        variant='contained'
-                        fullWidth
-                        sx={{...theme.components.MuiButton}}>
-                    수정하기
-                    </Button>
+                        {/* 장바구니 및 구매 버튼 */}
+                        <Grid container spacing={2} sx={{ marginTop: 2 }}>
+                        <Grid item xs={6}>
+                            <Button
+                                type="submit"
+                                variant='contained'
+                                fullWidth
+                                sx={{...theme.components.MuiButton}}>
+                            수정하기
+                            </Button>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Button
+                                type="button"
+                                onClick={handleDelete}
+                                variant='contained'
+                                fullWidth
+                                sx={{...theme.components.MuiButton}}>
+                            삭제하기
+                            </Button>
+                        </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <Button
-                        type="button"
-                        onClick={handleDelete}
-                        variant='contained'
-                        fullWidth
-                        sx={{...theme.components.MuiButton}}>
-                    삭제하기
-                    </Button>
-                </Grid>
-                </Grid>
-            </Grid>
-            </Grid>
+            </Form>
         </Formik>
     );
 }
